22b69e3a2261097402ddcf29452f65378cf33d45
eudaldmoya <eudald.moya.8@gmail.com>
2023-07-30 20:58:43 +0200

    Add constructor with pokemon fetch and main render method

--- src/ts/components/DetailComponent/DetailComponent.ts
+++ src/ts/components/DetailComponent/DetailComponent.ts
@@ -1,18 +1,35 @@
+import getPokemonData from "../../getData/getPokemonData.js";
+import { apiBaseUrl } from "../../globals/globals.js";
+import { type PokemonData } from "../../types.js";
 import Component from "../Component/Component.js";
 
 class DetailComponent extends Component {
+  private pokemonData: PokemonData;
+  private readonly id: number;
+
   constructor(parentElement: Element) {
     super(parentElement, "section", "detail-section");
-    const url = window.location.search;
-    const params = new URLSearchParams(url);
-    console.log(params.get("id"));
+
+    this.id = parseInt(localStorage.getItem("id")!, 10);
+
+    (async () => {
+      const pokemonData = await getPokemonData(`${apiBaseUrl}/${this.id}`);
+      this.pokemonData = pokemonData;
+
+      this.setVariables();
+    })();
   }
 
   public render(): void {
     super.render();
 
-    this.element.innerHTML = `<button class="back">back to home page</button>  
-    <article class= "card-detail"></article>`;
+    this.element.innerHTML = `<button class="button back">back to home page</button>  
+    <article class= "card-detail">
+      <img class="card__image">
+      <div class="card_text">
+        <h2 class="card__name"></h2>
+      </div>
+    </article>`;
 
     this.handleBackButton();
   }
@@ -23,6 +40,17 @@ class DetailComponent extends Component {
     backButtonElement.addEventListener("click", () => {
       window.location.href = "index.html";
     });
+  }
+
+  private setVariables(): void {
+    const imgElement = this.element.querySelector("img");
+    imgElement?.setAttribute(
+      "src",
+      this.pokemonData.sprites.other.dream_world.front_default
+    );
+
+    const nameElement = this.element.querySelector(".card__name")!;
+    nameElement.textContent = this.pokemonData.name;
   }
 }
 
